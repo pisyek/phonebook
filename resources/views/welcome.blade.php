ee0a59850e357263f17a3d9e7ed053bb8bec8f8e
Pisyek <pisyek@gmail.com>
2017-03-18 00:07:22 +0800

    Change title project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My Simple Phonebook App using Laravel
                 </div>
                 
                 <div class="links">
