4d39fbce429077a1b120cd7f3578109213fe863d
Pisyek <pisyek@gmail.com>
2017-03-18 00:27:08 +0800

    Add contact menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,15 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    Manage Contact <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                     <li><a href="{{ route('contacts') }}">View All</a></li>
+                                     <li><a href="{{ route('contacts.create') }}">Add New</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
