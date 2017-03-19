d458b754de58d96ed8f92edf92b8c702760eaeb6
Pisyek <pisyek@gmail.com>
2017-03-19 15:57:37 +0800

    fix alrt layout

--- resources/views/contact-create.blade.php
+++ resources/views/contact-create.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
 
-    @include('layout.alert')
-    
+    @include('layouts.alert')
+
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
