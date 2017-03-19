44780ca57be52053cb8befdd3a92eeea278ce4ef
Pisyek <pisyek@gmail.com>
2017-03-19 15:57:05 +0800

    add contact controller and alert box

--- resources/views/contact-create.blade.php
+++ resources/views/contact-create.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+
+    @include('layout.alert')
+    
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
---
+++ resources/views/layouts/alert.blade.php
@@ -0,0 +1,7 @@
+@if($message)
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="alert alert-success" role="alert">{{ $message }}</div>
+    </div>
+</div>
+@endif
