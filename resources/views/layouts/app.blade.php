8df1f8c3c656db2ef3325c1ed3f06b44ce1ba082
Pisyek <pisyek@gmail.com>
2017-03-23 23:38:20 +0800

    Add menu change password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,6 +71,9 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="{{ route('password.edit') }}">Change Password</a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
